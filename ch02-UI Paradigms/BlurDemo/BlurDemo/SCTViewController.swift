6c3bab3a9f9b64e0ce8a591458e094521471e778
wj2061 <wj2061@163.com>
2015-09-29 10:32:44 +0800

    add ch02-UI Paradigms

---
+++ ch02-UI Paradigms/BlurDemo/BlurDemo/SCTViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  BlurDemo
+//
+//  Created by WJ on 15/9/29.
+//  Copyright © 2015年 wj. All rights reserved.
+//
+
+import UIKit
+
+class SCTViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+//    var layer = CALayer()
+    let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.Light))
+
+    
+    @IBAction func buttonAction() {
+        /*
+        layer.frame=CGRectMake(80, 100, 160, 160)
+        view.layer.addSublayer(layer)
+        let scale = UIScreen.mainScreen().scale
+        
+        UIGraphicsBeginImageContextWithOptions(layer.frame.size, true, scale)
+        view.drawViewHierarchyInRect(view.frame, afterScreenUpdates: false)
+        var image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        if  let imageref = CGImageCreateWithImageInRect(image.CGImage, CGRectMake(layer.frame.origin.x*scale,
+                                                                                  layer.frame.origin.y*scale,
+                                                                                  layer.frame.size.width*scale,
+                                                                                  layer.frame.size.height*scale))
+        {
+           image = UIImage(CGImage: imageref)
+        }
+        */
+        // I gave up.
+        
+        if view.subviews.contains(blurEffectView){
+            blurEffectView.removeFromSuperview()
+        }else{
+            blurEffectView.frame = CGRectMake(80, 100, 160, 160)
+            view.addSubview(blurEffectView)
+        }
+    }
+}
+
