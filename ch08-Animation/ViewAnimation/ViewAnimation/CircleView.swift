dfa9ade4eec37663d41794d89acb5f3e1ac86455
wj2061 <h@hmatoMacBook-Air.local>
2015-10-07 00:50:18 +0800

    add ch08 - 01 ViewAnimation Demo

---
+++ ch08-Animation/ViewAnimation/ViewAnimation/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  ViewAnimation
+//
+//  Created by wj on 15/10/6.
+//  Copyright © 2015年 wj. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
