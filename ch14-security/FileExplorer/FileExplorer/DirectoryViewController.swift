d5d76b2b4154e2ce9819219129bb27825c3cb9f4
wj2061 <wj2061@126.com>
2015-11-09 00:15:07 +0800

    add ch-14

--- ch14-security/FileExplorer/FileExplorer/DirectoryViewController.swift
+++ ch14-security/FileExplorer/FileExplorer/DirectoryViewController.swift
@@ -9,87 +9,101 @@
 import UIKit
 
 class DirectoryViewController: UITableViewController {
-
+    
+    var  path:String?{
+        didSet{
+            if let st = path{
+                title = (st as NSString).lastPathComponent
+            }
+        }
+    }
+    
+    var contents = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        loadContents()
+        tableView.reloadData()
+    }
+    
+    func loadContents(){
+        if path == nil{
+            path  = "/"
+        }
+        let fileManager = NSFileManager()
+        
+        do {
+            contents = try  fileManager.contentsOfDirectoryAtPath(path!)
+        }catch let error {
+            print("error : \(error )")
+        }
+        print("contents: \(contents.count)")
+    }
+    
+    func isDirectoryAtEntryPath(entryPath:String)->Bool  {
+        let isDirectory = UnsafeMutablePointer<ObjCBool>.alloc(1)
+        isDirectory[0] = false
+        let fm = NSFileManager()
+        let fullPath = (path! as NSString).stringByAppendingPathComponent(entryPath)
+        fm.fileExistsAtPath(fullPath, isDirectory: isDirectory)
+        return Bool(isDirectory[0])
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return contents.count
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        let entryPath = contents[indexPath.row]
+        let fm = NSFileManager()
+        var attributes=[String:AnyObject]()
+        
+        do{
+            attributes = try  fm.attributesOfItemAtPath(entryPath)
+        }catch let error {
+            print("error = \(error)")
+        }
+        print("attributes = \(attributes)")
+        
+        let Permissions = attributes[NSFilePosixPermissions] == nil ? "" : "\(attributes[NSFilePosixPermissions]!)"
+        let name = attributes[NSFileOwnerAccountName] == nil ? "" : "\(attributes[NSFileOwnerAccountName]!)"
+        let id = attributes[NSFileOwnerAccountID] == nil ? "" : "\(attributes[NSFileOwnerAccountID]!)"
+        cell.textLabel?.text = "\(Permissions)  \(name)(\(id))  \((entryPath as NSString).lastPathComponent)"
+        
+        if isDirectoryAtEntryPath(entryPath){
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+            cell.selectionStyle = UITableViewCellSelectionStyle.Blue
+        }else{
+            cell.accessoryType = UITableViewCellAccessoryType.None
+            cell.selectionStyle = UITableViewCellSelectionStyle.None
+        }
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        let entrypath = contents[indexPath.row]
+        return isDirectoryAtEntryPath(entrypath) ? indexPath : nil
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let VC = segue.destinationViewController as! DirectoryViewController
+        
+        let entrypath = contents[tableView.indexPathForSelectedRow!.row]
+        let fullpath = (path! as NSString).stringByAppendingPathComponent(entrypath)
+        
+        VC.path = fullpath
     }
-    */
-
 }
