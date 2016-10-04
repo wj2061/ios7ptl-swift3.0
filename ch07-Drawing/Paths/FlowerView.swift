dc67e7706fd110de6f452f46e67480ab9c604c49
wj <wangjun@qeeniao.com>
2016-10-05 01:05:48 +0800

    ch07-01 : update to swift 3

--- ch07-Drawing/Paths/FlowerView.swift
+++ ch07-Drawing/Paths/FlowerView.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class FlowerView: UIView {
     override func awakeFromNib() {
-        contentMode = UIViewContentMode.Redraw
+        contentMode = UIViewContentMode.redraw
     }
     
-    override func drawRect(rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         let size = bounds.size
         let margin:CGFloat = 10
         let radius = rint( min(size.width-margin, size.height-margin)/4 )
@@ -22,33 +22,33 @@ class FlowerView: UIView {
         let xOffset = offset>0 ?  rint(margin/2) : -offset
         let yOffset = offset>0 ? offset : rint(margin/2)
         
-        UIColor.redColor().setFill()
+        UIColor.red.setFill()
         let path = UIBezierPath()
-        path.addArcWithCenter(CGPoint(x: radius * 2 + xOffset,
+        path.addArc(withCenter: CGPoint(x: radius * 2 + xOffset,
                                       y: radius + yOffset),
                               radius: radius,
                           startAngle: CGFloat(-M_PI),
                             endAngle: 0,
                            clockwise: true)
-        path.addArcWithCenter(CGPoint(x: radius * 3 + xOffset,
+        path.addArc(withCenter: CGPoint(x: radius * 3 + xOffset,
                                       y: radius * 2 + yOffset),
                              radius: radius,
                          startAngle: CGFloat(-M_PI_2),
                            endAngle: CGFloat(M_PI_2),
                           clockwise: true)
-        path.addArcWithCenter(CGPoint(x: radius * 2 + xOffset,
+        path.addArc(withCenter: CGPoint(x: radius * 2 + xOffset,
                                       y: radius * 3 + yOffset),
                               radius: radius,
                           startAngle: 0,
                             endAngle: CGFloat(M_PI),
                            clockwise: true)
-        path.addArcWithCenter(CGPoint(x: radius + xOffset,
+        path.addArc(withCenter: CGPoint(x: radius + xOffset,
                                       y: radius * 2 + yOffset),
                             radius: radius,
                         startAngle: CGFloat(M_PI_2),
                           endAngle: CGFloat(-M_PI_2),
                          clockwise: true)
-        path.closePath()
+        path.close()
         path.fill()
     }
 }
