20c5868ae6e6c8e587a0f36374d3017cebdcb862
wj <wangjun@qeeniao.com>
2016-10-05 01:09:50 +0800

    ch07-02 : update to swift 3

--- ch07-Drawing/Tranforms/FlowerTransformView.swift
+++ ch07-Drawing/Tranforms/FlowerTransformView.swift
@@ -10,27 +10,27 @@ import UIKit
 
 class FlowerTransformView: UIView {
     override func awakeFromNib() {
-        contentMode = UIViewContentMode.Redraw
+        contentMode = UIViewContentMode.redraw
     }
     
-    @inline(__always) func  CGAffineTransformMakeScaleTranslate (sx:CGFloat,sy:CGFloat,dx:CGFloat,dy:CGFloat)->CGAffineTransform{
-        return CGAffineTransformMake(sx, 0, 0, sy, dx, dy);
+    @inline(__always) func  CGAffineTransformMakeScaleTranslate (_ sx:CGFloat,sy:CGFloat,dx:CGFloat,dy:CGFloat)->CGAffineTransform{
+        return CGAffineTransform(a: sx, b: 0, c: 0, d: sy, tx: dx, ty: dy);
     }
    
-    override func drawRect(rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         let size = bounds.size
         let margin:CGFloat = 10
         
-        UIColor.redColor().set()
+        UIColor.red.set()
         let path = UIBezierPath()
-        path.addArcWithCenter(CGPointMake(0, -1), radius: 1, startAngle: CGFloat(-M_PI), endAngle: 0, clockwise: true)
-        path.addArcWithCenter(CGPointMake(1, 0), radius: 1, startAngle: CGFloat(-M_PI_2), endAngle: CGFloat(M_PI_2), clockwise: true)
-        path.addArcWithCenter(CGPointMake(0, 1), radius: 1, startAngle: 0, endAngle: CGFloat(M_PI), clockwise: true)
-        path.addArcWithCenter(CGPointMake(-1, 0), radius: 1, startAngle: CGFloat(M_PI_2), endAngle: CGFloat(-M_PI_2), clockwise: true)
+        path.addArc(withCenter: CGPoint(x: 0, y: -1), radius: 1, startAngle: CGFloat(-M_PI), endAngle: 0, clockwise: true)
+        path.addArc(withCenter: CGPoint(x: 1, y: 0), radius: 1, startAngle: CGFloat(-M_PI_2), endAngle: CGFloat(M_PI_2), clockwise: true)
+        path.addArc(withCenter: CGPoint(x: 0, y: 1), radius: 1, startAngle: 0, endAngle: CGFloat(M_PI), clockwise: true)
+        path.addArc(withCenter: CGPoint(x: -1, y: 0), radius: 1, startAngle: CGFloat(M_PI_2), endAngle: CGFloat(-M_PI_2), clockwise: true)
 
         let scale = floor((min(size.height, size.width) - margin)/4 )
         let tranform = CGAffineTransformMakeScaleTranslate(scale, sy: scale, dx: size.width/2, dy: size.height/2)
-        path.applyTransform(tranform)
+        path.apply(tranform)
         path.fill()
     }
 }
