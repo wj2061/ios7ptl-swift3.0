9200b3f7d22776beb034f5b867e2a7521180e0e8
wj <wangjun@qeeniao.com>
2016-10-06 16:43:23 +0800

    ch08-03: update to swift3

--- ch08-Animation/LayerAnimation/LayerAnimation/LayerAnimationViewController.swift
+++ ch08-Animation/LayerAnimation/LayerAnimation/LayerAnimationViewController.swift
@@ -9,34 +9,34 @@
 import UIKit
 
 class LayerAnimationViewController: UIViewController {
-    private let squareLayer = CALayer()
-    private let squareView = UIView()
+    fileprivate let squareLayer = CALayer()
+    fileprivate let squareView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        squareLayer.backgroundColor = UIColor.redColor().CGColor
+        squareLayer.backgroundColor = UIColor.red.cgColor
         squareLayer.frame = CGRect(x: 100, y: 100, width: 20, height: 20)
         view.layer.addSublayer(squareLayer)
         
-        squareView.backgroundColor = UIColor.blueColor()
+        squareView.backgroundColor = UIColor.blue
         squareView.frame = CGRect(x: 200, y: 100, width: 20, height: 20)
         view.addSubview(squareView)
         
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: Selector("drop:")))
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(LayerAnimationViewController.drop(_:))))
     }
 
-     func drop(gesture:UIGestureRecognizer){
+     func drop(_ gesture:UIGestureRecognizer){
         CATransaction.setAnimationDuration(2.0)
         squareLayer.position = CGPoint(x: 200, y: 250)
         
-        squareLayer.addAnim(NSNumber(double: 0), keypath: "opacity", duration: 2.0, delay: 0)
+        squareLayer.addAnim(NSNumber(value: 0 as Double), keypath: "opacity", duration: 2.0, delay: 0)
         
         squareView.center = CGPoint(x: 100, y: 250)
     }
 }
 
   private extension CALayer{
-     func addAnim(value:NSObject?,keypath:String,duration:CFTimeInterval,delay:CFTimeInterval){
+     func addAnim(_ value:NSObject?,keypath:String,duration:CFTimeInterval,delay:CFTimeInterval){
          CATransaction.begin()
          CATransaction.setDisableActions(true)
          setValue(value, forKeyPath: keypath)
@@ -44,11 +44,11 @@ class LayerAnimationViewController: UIViewController {
          anim.duration = duration
          anim.beginTime = CACurrentMediaTime()+delay
          anim.fillMode = kCAFillModeBoth
-         anim.fromValue = presentationLayer()?.valueForKey(keypath)
+         anim.fromValue = presentation()?.value(forKey: keypath)
          anim.toValue = value
          anim.autoreverses = true
          anim.repeatCount = Float.infinity
-         addAnimation(anim, forKey: keypath)
+         add(anim, forKey: keypath)
          CATransaction.commit()
     }
 }
