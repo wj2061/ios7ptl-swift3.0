97775a4064643e3ac7cb64800ff5e81562ecb935
wj2061 <wj2061@126.com>
2017-09-14 17:42:00 +0800

    update ch21-03 to swift 3

--- ch21-Text/Exclusion/Exclusion/ViewController.swift
+++ ch21-Text/Exclusion/Exclusion/ViewController.swift
@@ -13,22 +13,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let path = NSBundle.mainBundle().pathForResource("sample.txt", ofType: nil)
+        let path = Bundle.main.path(forResource: "sample.txt", ofType: nil)
         var st:String?
         do {
-             st  = try NSString(contentsOfFile: path!, encoding: NSUTF8StringEncoding) as String
+             st  = try NSString(contentsOfFile: path!, encoding: String.Encoding.utf8.rawValue) as String
         }catch let error as NSError{
             print(error.description)
             return
         }
         
         textView.text = st
-        textView.textAlignment = .Justified
+        textView.textAlignment = .justified
         
         let bounds = self.view.bounds
-        let width  = CGRectGetWidth(bounds)
-        let height = CGRectGetHeight(bounds)
-        let rect   = CGRectInset(bounds, width/4, height/4)
+        let width  = bounds.width
+        let height = bounds.height
+        let rect   = bounds.insetBy(dx: width/4, dy: height/4)
         let exclusionPath = UIBezierPath(roundedRect: rect, cornerRadius: width/10)
         textView.textContainer.exclusionPaths = [exclusionPath]
     }
